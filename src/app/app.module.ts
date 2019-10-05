8126b901554adef083d604e989584fa82ddd1eff
Kimc17 <Kimc17@gmail.com>
2019-10-05 14:05:03 -0600

    Finalizados los registros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import {FormsModule} from '@angular/forms';
 //import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -39,7 +41,8 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule,
 
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  show: boolean = true;
+ 
+  comprador: any ={
+    nombre:'',
+    genero:'',
+    nacimiento: '',
+    ingresos: '',
+    ocupacion: '',
+    residencia: {
+        provincia: 'Molecule Man',
+        canton: 29,    
+    },
+      
+  };
+  vendedor: any ={
+    nombre:'',
+    nacimiento: '',
+  };
   constructor() { }
 
   ngOnInit() {
   }
 
+  agregar_comprador(){
+    console.log(this.comprador);
+    
+    this.comprador={
+      nombre:'',
+      genero:'',
+      nacimiento: '',
+      ingresos: '',
+      ocupacion: '',
+      residencia: {
+          provincia: '',
+          canton: '',  
+          distrito: '',  
+      },
+          
+
+    }
+  
+    
+  }
+  agregar_vendedor(){
+    console.log(this.vendedor);
+    
+    this.vendedor={
+      nombre:'',
+      nacimiento: '',
+    };
+    console.log(this.vendedor);
+  }
+  
+s
 }
