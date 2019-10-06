1a028d51872430b989ef6816921fbad4d02756e9
Kimc17 <Kimc17@gmail.com>
2019-10-06 16:15:43 -0600

    Se agrega vista de filtros

--- src/app/busqueda/busqueda.component.ts
+++ src/app/busqueda/busqueda.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusquedaComponent implements OnInit {
   propiedades:Array<any>=[
-    { titulo: 'Casa grande', foto:'https://cdn.pixabay.com/photo/2016/11/18/17/46/architecture-1836070_960_720.jpg', ubicacion: 'Sus sueños', precio: '70000000'},
-    { titulo: 'Apartamento individual', foto:'https://cdn.pixabay.com/photo/2016/09/22/11/55/kitchen-1687121__340.jpg', ubicacion: 'TEC', precio: '300000'},
-    { titulo: 'Casa pequeña', foto:'https://cdn.pixabay.com/photo/2013/08/30/12/56/holiday-house-177401__340.jpg',ubicacion: 'al frente', precio: '450000'},
-    { titulo: 'Habitación compartida', foto:'https://cdn.pixabay.com/photo/2015/03/26/09/42/bedroom-690129__340.jpg', ubicacion: 'Cerca del TEC', precio: '100000'},
+    { titulo: 'Casa grande', foto:'https://cdn.pixabay.com/photo/2016/11/18/17/46/architecture-1836070_960_720.jpg', ubicacion: 'Sus sueños', precio: '$70000'},
+    { titulo: 'Apartamento individual', foto:'https://cdn.pixabay.com/photo/2016/09/22/11/55/kitchen-1687121__340.jpg', ubicacion: 'TEC', precio: '$600'},
+    { titulo: 'Casa pequeña', foto:'https://cdn.pixabay.com/photo/2013/08/30/12/56/holiday-house-177401__340.jpg',ubicacion: 'al frente', precio: '$900'},
+    { titulo: 'Habitación compartida', foto:'https://cdn.pixabay.com/photo/2015/03/26/09/42/bedroom-690129__340.jpg', ubicacion: 'Cerca del TEC', precio: '$200'},
   ];
   constructor() { }
 
